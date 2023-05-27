d2e1261526e4eb376e4ac97638b7ac938b0dd911
Anh <anhnpgcs200323@fpt.edu.vn>
2023-05-27 15:45:23 +0700

    end of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,8 +13,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { SharedModule } from './_modules/shared.module';
 
 
 
@@ -35,10 +34,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
-    })
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
